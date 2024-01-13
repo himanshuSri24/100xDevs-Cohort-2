5563df8963ae3eff80e63bc642508f3be41530ea
himanshuSri24 <himanshu20032002@gmail.com>
2024-01-14 01:50:49 +0530

    feat(Week_0): expenditure-analysis.js solved

--- 0-1/Week_0/Assignments/01-js/easy/expenditure-analysis.js
+++ 0-1/Week_0/Assignments/01-js/easy/expenditure-analysis.js
@@ -13,8 +13,30 @@
   Output - [{ category: 'Food', totalSpent: 10 }] // Can have multiple categories, only one example is mentioned here
 */
 
+/**
+ * Calculates the total amount spent for each category in the given transactions.
+ * @param {Array<Object>} transactions - The array of transactions.
+ * @returns {Array<Object>} - An array of objects containing the category and the total amount spent for each category.
+ */
 function calculateTotalSpentByCategory(transactions) {
-  return [];
+  const result = [];
+  // for each transaction, check if category exists in result
+  // if not, add it to result
+  // if exists, add price to totalSpent
+  transactions.map((transaction) => {
+    const index = result.findIndex(
+      (item) => item.category === transaction.category
+    );
+    if (index === -1) {
+      result.push({
+        category: transaction.category,
+        totalSpent: transaction.price,
+      });
+    } else {
+      result[index].totalSpent += transaction.price;
+    }
+  });
+  return result;
 }
 
 module.exports = calculateTotalSpentByCategory;
