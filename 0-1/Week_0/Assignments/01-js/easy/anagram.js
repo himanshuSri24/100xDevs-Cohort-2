48c42d2f05548053cc097b1c667a5125bc0f45ff
himanshuSri24 <himanshu20032002@gmail.com>
2024-01-14 01:42:15 +0530

    feat(Week_0): anagram.js solved

--- 0-1/Week_0/Assignments/01-js/easy/anagram.js
+++ 0-1/Week_0/Assignments/01-js/easy/anagram.js
@@ -4,8 +4,20 @@
   - A word, phrase, or name formed by rearranging the letters of another, such as spar, formed from rasp.
 */
 
+/**
+ * Checks if two strings are anagrams.
+ *
+ * @param {string} str1 - The first string.
+ * @param {string} str2 - The second string.
+ * @returns {boolean} Returns true if the strings are anagrams, false otherwise.
+ */
 function isAnagram(str1, str2) {
-
+  // convert strings to lowercase, split them into arrays, sort them, and join them back into strings
+  // then compare the strings
+  return (
+    str1.toLowerCase().split("").sort().join("") ===
+    str2.toLowerCase().split("").sort().join("")
+  );
 }
 
 module.exports = isAnagram;
